4df4af6593b298dfc77b54f6cc123a5d3668d46a
charlie-laptop <cww324@gmail.com>
2025-05-16 15:46:33 -0500

    umm just some files that didn't get saves?

--- src/pages/CreateTripsForMatch.jsx
+++ src/pages/CreateTripsForMatch.jsx
@@ -1,4 +1,8 @@
-// src/pages/CreateTripForMatch.jsx
+// This page is probably old and outdated and NOT USED
+// THIS PAGE IS NOT USED ANYMORE
+// WOOHOOO
+
+
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import {
